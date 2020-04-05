29dca5b11d6f50f866a8537b3e6a0eb3606483f9
James Goodliffe <jxg523@cs.bham.ac.uk>
2020-04-05 17:26:24 +0100

    Add AssetsTD.swift

---
+++ AssetClient/ViewControllers/TableDelegates/AssetsTD.swift
@@ -0,0 +1,20 @@
+//
+//  AssetsTD.swift
+//  Production Manager Pro
+//
+//  Created by Jamie Goodliffe on 04/04/2020.
+//  Copyright © 2020 Jamie Goodliffe. All rights reserved.
+//
+
+import Cocoa
+import AppKit
+import SwiftyJSON
+import Alamofire
+
+class AssetsTD: NSObject, NSTableViewDelegate, NSTableViewDataSource{
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 1
+    }
+
+}
